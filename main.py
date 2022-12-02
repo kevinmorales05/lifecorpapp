23a9fdd10a5d624c01562e7f55ec76cc30a7d0ed
Kevin Morales <kevin.morales05@epn.edu.ec>
2022-12-02 15:45:17 -0500

    Finish login with Firebase and signup with Firebase

--- main.py
+++ main.py
@@ -8,44 +8,79 @@ TO DO: use as a environmental variables.
 #import firebase
 import firebase_admin
 from firebase_admin import credentials
-#import db from firebase in order to implement reading
+# import db from firebase in order to implement reading
 from firebase_admin import db
 #configure and initializafirebase
-cred = credentials.Certificate("./config/byu-cs111-firebase-adminsdk-ve0lu-d5670a8f24.json")
-db = "https://byu-cs111-default-rtdb.firebaseio.com/";
-firebase_admin.initialize_app(cred, {'databaseURL': db} )
+cred = credentials.Certificate(
+    "./config/byu-cs111-firebase-adminsdk-ve0lu-d5670a8f24.json")
+dbURL = "https://byu-cs111-default-rtdb.firebaseio.com/"
+firebase_admin.initialize_app(cred, {'databaseURL': dbURL})
+# Reference of the Database Users
+refUsers = db.reference("/users")
+# Reference of the Database Products
+refProducts = db.reference("/products")
 
 
 
 
 def login():
-        validate = True
-        while validate == True:
-            user = str(input("Write your email address: "))
-            pwd = str(input("Write your password: "))
-            loginWithFirebase(user,pwd)
+    validate = True
+    while validate == True:
+        user = str(input("Write your email address: "))
+        pwd = str(input("Write your password: "))
+        #call login with Firebase
+        userLogged = loginWithFirebase(user, pwd)
+        #validate userLogged
+        if userLogged != None :
+            validate = False
+            homeMenu(userLogged)
+        
+
 
 def signUp():
     name = str(input("Write your name please: "))
-    user = str(input("Write your email address: "))
+    email = str(input("Write your email address: "))
     pwd = str(input("Write your password: "))
-    signUpWithFirebase(user,name, pwd)
+    signUpWithFirebase(email, name, pwd)
 
 def logOut():
     print("Thanks for using Life Corp App")
 
+
 def errorInvalidOption():
     print("Sorry, this is an incorrect option...")
 
+
 def loginWithFirebase(user, pwd):
     print(f"Validando... User: {user}")
-    #success, go to homeMenu
-    #failure case, return to login
+    # success, go to homeMenu
+    # failure case, return to login
+    userDatabase = refUsers.get()
+    for key, value in userDatabase.items():
+        if (value['email'] == user and value['pwd'] == pwd):
+            print()
+            print("The login was successfull")
+            print(f"************************")
+            print()
+            return value
+        else:
+            print("Invalid credentials, try again...")
+            return None
+    #print("This is the obtained email",getEmail)
 
-def signUpWithFirebase(user,name, pwd):
+def signUpWithFirebase(email, name, pwd):
     print(f"Creando usuario... {name}")
-    #success redirect to login
-    #failure case redirect to startMenu
+    # success redirect to login
+    # failure case redirect to startMenu
+    user = {
+        "name": name,
+        "email": email,
+        "pwd": pwd
+    }
+    refUsers.push().set(user)
+    print("Account created successfully!")
+    
+
 
 def startMenu():
     begin = True
@@ -54,14 +89,14 @@ def startMenu():
         print("1. Login")
         print("2. Create a new account")
         print("0. Exit")
-        op = int(input("Choose an option to continue: "))
-        if op == 0:
+        op = input("Choose an option to continue: ")
+        if op == '0':
             begin = False
             logOut()
-        elif op == 1:
+        elif op == '1':
             print("Call login function")
             login()
-        elif op == 2:
+        elif op == '2':
             print("Call sign up function")
             signUp()
         else:
@@ -70,7 +105,7 @@ def startMenu():
 
 # user is an object with this form
 def homeMenu(user):
-    print(f"Welcome {user.name}!")
+    print(f"Welcome {user['name']}!")
     begin = True
     while begin == True:
         print("*** Life Corporation App ***")
@@ -91,12 +126,6 @@ def homeMenu(user):
             signUp()
         else:
             errorInvalidOption()
-        
-
-        
 
 
-        
-startMenu() 
-    
-
+startMenu()
--- readme.md
+++ readme.md
@@ -5,3 +5,36 @@ pip install --upgrade pip
 pip install firebase_admin
 
 run python3 main.py
+
+
+
+
+
+*** Database  Collections ***
+Users Collection
+{
+	"UserID1":
+	{
+		"name": "Kevin Morales",
+		"email": "kevin@gmail.com",
+		"password": "prueba12345"
+	},
+	"UserID2":
+	{
+		"name": "Juan Morales",
+		"email": "juan@gmail.com",
+		"password": "prueba12345"
+	},
+    "UserID3":
+	{
+		"name": "Andres Morales",
+		"email": "andres@gmail.com",
+		"password": "prueba12345"
+	},
+    "UserID4":
+	{
+		"name": "Leonel Morales",
+		"email": "leo@gmail.com",
+		"password": "prueba12345"
+	},
+}
